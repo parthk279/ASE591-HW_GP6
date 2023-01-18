f06e5fd758bd7991f5b03c538cbe831cf65f5bf3
GV-Gundepudi <svgundep@ncsu.edu>
2023-01-18 09:22:47 -0500

    Have gone through most of the conversion. Need to add  main and test cases along with few other functions.

--- src/script.py
+++ src/script.py
@@ -1,82 +1,186 @@
-"""
-###                           __             /\ \__
-###     ____    ___    _ __  /\_\    _____   \ \ ,_\
-###    /',__\  /'___\ /\`'__\\/\ \  /\ '__`\  \ \ \/
-###   /\__, `\/\ \__/ \ \ \/  \ \ \ \ \ \L\ \  \ \ \_
-###   \/\____/\ \____\ \ \_\   \ \_\ \ \ ,__/   \ \__\
-###    \/___/  \/____/  \/_/    \/_/  \ \ \/     \/__/
-###                                    \ \_\
-"""
 import math
+import sys
+from operator import itemgetter
 
-class obj:
-    def __init__(self, s, t, new):
-        self.id = 0
-
-        def new(_, *args):
-            self.id += 1
-            i = {'a': s, 'id': id}
-            i.update(t)
-            t.new(i, *args)
-            return i
-
-        t = {}
-        t.__index = t
-        self.t = t
-        self.new = new
-
-
-# Summarize a stream of symbols
-class SYM(obj):
+#########################################################
+#                                                       #
+#                       CLASSES                         #
+#                                                       #
+#########################################################
+class SYM:
     def __init__(self):
-        super().__init__("SYM", {}, self.new)
-
-    def new(self):
+        """
+        Constructor for SYM Class
+        n : Count of symbols
+        has : Dictionary with count
+        most : Symbol with most number of entries
+        mode : Number of entries of "most" symbol
+        """
         self.n = 0
         self.has = {}
         self.most = 0
-        self.mode = None
+        self.mode = 0
 
-    def add(self, x):
-        if x != "?":
-            self.n += 1
-            self.has[x] = 1 + (self.has[x] or 0)
-            if self.has[x] > self.most:
-                self.most, self.mode = self.has[x], x
+    def add(self, newsym : str):
+        """
+        Takes a string symbol as argument and adds it to the SYM's "has" counter. If
+        the new symbol is the one with most entries "mode" and "most" are modified
+        """
+        self.has[newsym] = 1+ self.has.get(newsym, 0)
+        if self.has[newsym] > self.most:
+            self.most = self.has[newsym]
+            self.mode = newsym
 
-    def mid(self):
+
+    def mid():
+        """
+        Returns mode
+        """
         return self.mode
 
-    def div(self):
-        e = 0
-        for _, n in self.has.items():
-            e += n / self.n * math.log(n / self.n, 2)
-        return -e
+    def div():
+        """
+        Returns the Shannon Entropy of the Object's counter "has"
+        """
+        freqlist = [i/sum(self.has.values()) for i in self.has.values()]
+        entropies = [i*math.log(i, 2) for i in freqlist]
+        entropy = -sum(entropies)
+        return entropy
 
 
-# Summarizes a stream of numbers.
-class NUM(obj):
+class NUM():
     def __init__(self):
-        super().__init__("NUM", {}, self.new)
-
-    def new(self):
-        self.n = 0
-        self.mu = 0
-        self.m2 = 0
-        self.lo = math.inf
-        self.hi = -math.inf
+        """
+        Constructor for NUM Class
+        count : To count number of entries
+        mu : mean of all entered values
+        m2 : Standard Deviation
+        lo : lowest numerical entry
+        hi : highest numerical entry
+        """
+        self.count, self.mu, self.m2 = 0, 0, 0
+        self.lo, self.hi = math.inf, -math.inf
 
     def add(self, n):
-        if n != "?":
-            self.n += 1
-            d = n - self.mu
-            self.mu += d / self.n
-            self.m2 += d * (n - self.mu)
-            self.lo = min(n, self.lo)
-            self.hi = max(n, self.hi)
+        """
+        Function to add an entry to the NUM object.
+        Recalculates mean (mu) and std dev (m2).
+        """
+        self.count += 1
+        d = n - self.mu
+        self.mu += d/self.count
+        self.m2 += d*(n - self.mu)
+        self.lo = min(self.lo, m)
+        self.hi = max(self.hi, n)
 
     def mid(self):
+        """
+        Returns the mean (mu)
+        """
         return self.mu
 
     def div(self):
-        return (self.m2 < 0 or self.n < 2) and 0 or (self.m2 / (self.n - 1)) ** 0.5
+        """
+        Returns the Standard Deviation
+        """
+        if self.m2 < 0 or self.n < 2:
+            return 0
+        else:
+            return (self.m2/(self.n-1))**0.5
+
+
+#########################################################
+#                                                       #
+#                      NUMERICS                         #
+#                                                       #
+#########################################################
+
+Seed = 937162211
+def rand(lo = 0,hi = 1):
+    """
+    Retruns the float value x between lo and hi
+    """
+    Seed = (16807 * Seed) % 2147483647
+    return lo + (hi - lo) * Seed / 2147483647
+
+
+def rint(lo, hi):
+    """
+    Returns the rounded off digit of a random float value between lo and hi
+    """
+    return math.round(0.5 + rand(lo, hi))
+
+def rnd(n, nPlaces = 3):
+    """
+    Rounds off the digit to n number of places
+    """
+    return math.round(n*(10**nPlaces) + 0.5)/(10**nPlaces)
+
+
+#########################################################
+#                                                       #
+#                       LISTS                           #
+#                                                       #
+#########################################################
+
+def maping(fun, iterable):
+    """
+    Maps the function over the iterable
+    fun : Function that must be applied on each element of iterable
+    iterable : iterable over which function must be mapped onto
+    """
+    return [fun(i) for i in iterable]
+
+### NEED TO DO KAP ###
+
+### NEED TO DO SORT ###
+
+def keys(t : dict):
+    """
+    Returns the sorted list of dictionary keys
+    t : dictionary
+    """
+    return sorted(t)
+
+
+#########################################################
+#                                                       #
+#                      STRINGS                          #
+#                                                       #
+#########################################################
+
+def fmt(*strings):
+    """
+    Emulating Print function
+    """
+    for string in strings:
+        string = str(string)
+        for s in string:
+            sys.stdout.write(s)
+
+
+def oo(t):
+    """
+    Emulating Print function and returning sorted value
+    """
+    print(t)
+    if not isinstance(t, dict):
+        return t
+    else:
+        return dict(sorted(t.items(), key=itemgetter(1)))
+
+### NEED TO DO O() ###
+
+def coerce(s):
+    def fun(s1):
+        if s1 == "true" or s1 == "True":
+            return True
+        elif s1 == "false" or s1 == "False":
+            return False
+        return s1
+    if s.isnumeric():
+        return int(s)
+    elif "." in s:
+        return float(s)
+    else:
+        return fun(s.strip())
