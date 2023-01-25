35d9eb8ca71ad4510975207c46f4d68e3fda6ef5
GV-Gundepudi <svgundep@ncsu.edu>
2023-01-25 11:47:08 -0500

    Created Data Class

---
+++ src/HW2/data.py
@@ -0,0 +1,28 @@
+# from misc import *
+# from row import ROW
+# from col import COLS
+
+class DATA:
+    def __init__(self, src):
+        """
+        A container for self.rows to be summarized in self.cols
+        """
+        self.rows = []
+        self.cols = None
+        if isinstance(src, str):
+            csv(self, self.add)
+        else:
+            for i in src:
+                self.add(i)
+
+    def add(self, t):
+        """
+          Add a new row and update the column headers
+        """
+        if self.cols:
+            if isinstance(t, list):
+                t = ROW(t)
+            self.rows.append(t)
+            self.cols.add(t)
+        else:
+            self.cols = COLS(t)
