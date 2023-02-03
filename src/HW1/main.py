9196d610dfd14d109816de2ee78ff32e534c4c89
Harshitha.Paruchuri <59336190+Hp-Harshitha-hp@users.noreply.github.com>
2023-02-02 21:09:07 -0500

    Update main.py

--- src/HW1/main.py
+++ src/HW1/main.py
@@ -2,31 +2,30 @@ import sys, re, math
 from test_all import *
 from misc import *
 from config import *
-
+  
 def main():
-  saved, fails={},0
-  for k,v in cli(settings(help)).items():
-      the[k] = v
-      saved[k] = v
-  if the['help'] == True:
-      print(help)
-  else:
-    for what, fun in egs.items():
-        if the['go'] == 'all' or the['go'] == what:
-          for k,v in saved.items():
-              the[k] = v
-          Seed = the['seed']
-          if egs[what]() == False:
-              fails += 1
-              print('❌ Fail:', what)
-          else:
-              print('✅ Pass:', what)
-  sys.exit(fails)
+    saved,fails={},0
+    for k,v in cli(settings(help)).items():
+        the[k] = v
+        saved[k] = v
+    if the['help'] == True:
+        print(help)
+    else:
+        for what, fun in egs.items():
+            if the['go'] == 'all' or the['go'] == what:
+                for k,v in saved.items():
+                    the[k] = v
+                Seed = the['seed']
+                if egs[what]() == False:
+                    fails += 1
+                    print('❌ fail:', what)
+                else:
+                    print('✅ pass:', what)
+    sys.exit(fails)
 
-
-if __name__ == "__main__":
-  eg('the', "testng the", test_the)
-  eg('rand', "testing Random function", test_rand)
-  eg('sym', "testing the sym class", test_sym)
-  eg('num', "testing the num class", test_num)
-  main()
+if __name__ == '__main__':
+    eg('the', 'show settings', test_the)
+    eg('rand', 'generate, reset, regenerate same', test_rand)
+    eg('sym', 'check syms', test_sym)
+    eg('num', 'check nums', test_num)
+    main()
