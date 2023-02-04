5cd21440b3ec02c25d460af4e054a2cfcb76907f
Surya Sashank Gundepudi (GV) <66325251+suryasashankgundepudi@users.noreply.github.com>
2023-02-04 18:16:40 -0500

    Update misc.py

--- src/HW2/misc.py
+++ src/HW2/misc.py
@@ -3,6 +3,19 @@ from config import *
 from operator import itemgetter
 import os
 
+def csv(fileName, fun):
+    """
+    Function for reading the csv file and applying a function over the text in csv file
+    """
+
+    if os.path.exists(fileName) and fileName.endswith(".csv"):
+        with open(fileName, "r", encoding="utf-8") as file:
+            for _, row in enumerate(file):
+                r = list(map(coerce, row.strip().split(",")))
+                fun(r)
+    else:
+        print("File does not exist at : ", fileName)
+        return 0
 
 def misc(fun, iterable):
     """
