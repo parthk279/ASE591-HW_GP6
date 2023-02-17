688e259ad188a6a5deb13861633701d6fac5e36a
Harshitha <harshitha.paruchurii@gmail.com>
2023-02-16 22:45:10 -0500

    HW4-Config

--- src/HW4/config.py
+++ src/HW4/config.py
@@ -2,25 +2,20 @@ the = {
     'dump': False,
     'go': None,
     'seed': 937162211,
-    "file": "../../etc/data/auto93.csv",
-    "Far": 0.95,
-    "min": 0.5,
-    "p": 2,
-    "Sample": 512
 }
-egs = {}
-help = '''USAGE:   python main.py [OPTIONS] [-g ACTION]
-    OPTIONS:
-    -d  --dump  on crash, dump stack = false
-    -g  --go    start-up action      = data
-    -F  --Far     distance to "faraway"  = .95
-    -g  --go      start-up action        = all
-    -h  --help    show help              = false
-    -m  --min     stop clusters at N^min = .5
-    -p  --p       distance coefficient   = 2
-    -s  --seed    random number seed     = 937162211
-    -S  --Sample  sampling data size     = 512
+
+help = '''USAGE: python main.py  [OPTIONS] [-g ACTION]
+OPTIONS:
+  -d  --dump    on crash, dump stack   = false
+  -f  --file    name of file           = ../etc/data/repgrid1.csv
+  -g  --go      start-up action        = data
+  -h  --help    show help              = false
+  -p  --p       distance coefficient   = 2
+  -s  --seed    random number seed     = 937162211
     '''
+
+egs = {}
+
 Seed = 937162211
 
 n = 0
