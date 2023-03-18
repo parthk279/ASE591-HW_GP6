e102c96025950b01b859330bfdbff364b215269f
Parth Katlana <parthk279>
2023-03-18 00:13:06 -0400

    added a new test for xpln

--- src/HW6/test_all_HW4.py
+++ src/HW6/test_all_HW4.py
@@ -150,3 +150,18 @@ def test_dist():
     for row in data.rows:
         num.add(data.dist(row, data.rows[1]))
     print({'lo' : num.lo, 'hi' : num.hi, 'mid' : rnd(num.mid()), 'div' : rnd(num.div())})   
+    
+def test_xpln():
+    data = DATA(the['file'])
+    best,rest,evals = data.sway()
+    rule,most= data.xpln(best,rest)
+    print("\n-----------\nexplain=", data.showRule(rule))
+    selects = data.selects(rule,data.rows)
+    data_selects = [s for s in selects if s!=None]
+    data1= data.clone(data_selects)
+    print("all               ",data.stats('mid', data.cols.y, 2),data.stats('div', data.cols.y, 2))
+    print("sway with",evals,"evals",best.stats('mid', best.cols.y, 2),best.stats('div', best.cols.y, 2))
+    print("xpln on",evals,"evals",data1.stats('mid', data1.cols.y, 2),data1.stats('div', data1.cols.y, 2))
+    top,_ = data.betters(len(best.rows))
+    top = data.clone(top)
+    print("sort with",len(data.rows),"evals",top.stats('mid', top.cols.y, 2),top.stats('div', top.cols.y, 2))    
