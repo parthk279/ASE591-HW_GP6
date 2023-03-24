d39c715e5d09c8f60290c32c060b6ed2cea828ab
Parth Katlana <parthk279>
2023-03-23 23:30:04 -0400

    removed olf cliffdelta

--- src/HW7/misc.py
+++ src/HW7/misc.py
@@ -246,25 +246,6 @@ def merge(col1,col2):
     new.hi = max(col1.hi, col2.hi) 
   return new      
 
-def cliffsDelta(ns1,ns2):
-    if len(ns1) > 256:
-        ns1 = many(ns1,256)
-    if len(ns2) > 256:
-        ns2 = many(ns2,256)
-    if len(ns1) > 10*len(ns2):
-        ns1 = many(ns1,10*len(ns2))
-    if len(ns2) > 10*len(ns1):
-        ns2 = many(ns2,10*len(ns1))
-    n,gt,lt = 0,0,0
-    for x in ns1:
-        for y in ns2:
-            n = n + 1
-            if x > y:
-                gt = gt + 1
-            if x < y:
-                lt = lt + 1
-    return abs(lt - gt)/n > the['cliffs']  
-
 def prune(rule, maxSize):
     n=0
     for txt,ranges in rule.items():
